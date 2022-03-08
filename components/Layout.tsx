f56d4e60b4703d877e7df44b60401db2e93db227
jogiter <jogiter.g@gmail.com>
2022-03-08 19:08:24 +0800

    feat: update layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -11,7 +11,7 @@ export default function Layout({ children, footer = true }: Props) {
   return (
     <div>
       <Appbar></Appbar>
-      <div className='text-center'>
+      <div className='text-center p-4'>
         {children}
       </div>
       {
