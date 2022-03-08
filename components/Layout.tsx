150ad3e44656ec7f34a667e5623eac80770f5483
jogiter <jogiter.g@gmail.com>
2022-03-08 19:06:47 +0800

    feat: add layout

---
+++ components/Layout.tsx
@@ -0,0 +1,22 @@
+import { ReactNode } from 'react'
+import Appbar from './AppBar'
+import Footer from './Footer'
+
+export type Props = {
+  children?: ReactNode
+  footer?: boolean
+}
+
+export default function Layout({ children, footer = true }: Props) {
+  return (
+    <div>
+      <Appbar></Appbar>
+      <div className='text-center'>
+        {children}
+      </div>
+      {
+        footer ? <Footer></Footer> : ''
+      }
+    </div>
+  )
+}
